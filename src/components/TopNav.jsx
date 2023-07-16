b5e10c8d86ed9e16cea5c212538b49f2607ce897
mady1711 <m.kamani@iitg.ac.in>
2023-07-16 13:39:51 +0530

    Final commit

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { FaBars, FaUserTie } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const TopNav = ({ title, onOpen }) => {
+  return (
+    <Box px="4" bg="white">
+      <HStack maxW="70rem" h="16" justify="space-between" mx="auto">
+        <Icon
+          as={FaBars}
+          onClick={onOpen}
+          display={{
+            base: "block",
+            lg: "none",
+          }}
+        />
+        <Heading fontWeight="medium" fontSize="28px">
+          {title}
+        </Heading>
+
+        <Menu>
+          <MenuButton>
+            <Icon as={FaUserTie} fontSize="24px" />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Logout</MenuItem>
+            <Link to="/support">
+              <MenuItem>
+                Support
+              </MenuItem>
+            </Link>
+          </MenuList>
+        </Menu>
+      </HStack>
+    </Box>
+  );
+};
+
+export default TopNav;
